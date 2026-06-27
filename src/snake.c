aa740d9400b293e715a9a74cf7eec7c64e4960a8
jgdecampos <jdecamposnassar@gmail.com>
2026-06-26 23:48:24 -0300

    rodando

--- src/snake.c
+++ src/snake.c
@@ -134,32 +134,4 @@ bool snake_hits_rect(const Snake *snake, int16_t x, int16_t y, int16_t width, in
 
     return false;
 }
-#include "snake.h"
-#include "wasm4.h"
-#include <stdlib.h>
 
-void snake_init(struct snake *snake) {
-    if(snake->body != NULL)
-    {
-            free(snake->body);
-            snake->body = NULL;
-    }
-    snake->length = 0;
-}
-
-void snake_push(struct snake *snake, struct point p) {
-    struct point* body = realloc(snake->body, sizeof(struct point) * (snake->length+1));
-    if(body)
-    {
-        snake->body = body;
-        snake->body[snake->length++] = p;
-    }
-}
-
-void snake_draw(struct snake *snake) {
-    *DRAW_COLORS = 3;
-    for(size_t i = 0; i < snake->length; ++i)
-    {
-        rect(snake->body[i].x*8, snake->body[i].y*8,8,8);
-    }
-}
